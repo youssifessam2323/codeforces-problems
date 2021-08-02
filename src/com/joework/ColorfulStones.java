7e31f404513b8efa6dd965d48b92cc2ec2c5c7d9
youssifessam2323 <yousfessaam2323@gmail.com>
2021-08-02 11:12:47 +0200

    solving ColorfulStones problem

---
+++ src/com/joework/ColorfulStones.java
@@ -0,0 +1,30 @@
+package com.joework;
+
+import java.util.Scanner;
+
+public class ColorfulStones {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String wordOne = sc.nextLine();
+        String wordTwo = sc.nextLine();
+
+        int i = 0, j = 0;
+
+        while(j < wordTwo.length()){
+
+            if(wordTwo.charAt(j) == wordOne.charAt(i)){
+                i++;
+                j++;
+            }else {
+              j++;
+            }
+
+        }
+
+        System.out.println(i + 1);
+
+
+    }
+}
