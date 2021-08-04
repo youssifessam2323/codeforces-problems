e117428de429ae903722170017c5f9c8b4d0b427
youssifessam2323 <yousfessaam2323@gmail.com>
2021-08-04 14:07:07 +0200

    solving word capitalization problem

---
+++ src/com/joework/WordCapitalization.java
@@ -0,0 +1,19 @@
+package com.joework;
+
+import java.util.Scanner;
+
+public class WordCapitalization {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        StringBuilder word = new StringBuilder(sc.nextLine());
+        char c = word.charAt(0);
+        if(c >= 97) {
+            c -= 32;
+            word.setCharAt(0, c);
+        }
+        System.out.println(word);
+    }
+
+}
